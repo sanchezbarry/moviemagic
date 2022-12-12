961f991888db7017444723416e500e38569c6e36
Sanchez <sanchezbarry@gmail.com>
2022-12-12 21:07:23 +0800

    branched to dev, successfully pulled API data

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,33 @@ import Main from './Main';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+type Movie = {
+  name: string;
+  title: string;
+};
+
+function useMovies() {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await fetch('https://remarkable-bombolone-51a3d9.netlify.app/.netlify/functions/movies');
+      const data = await response.json();
+      setMovies(data);
+    }
+    
+    fetchMovies();
+  }, []);
+  
+  return movies;
+}
+
 export default function App() {
+  const movies = useMovies();
+
   return (
     <>
       <Navbar bg="dark" variant="dark">
@@ -26,6 +50,15 @@ export default function App() {
         <hr />
         <Main />       
       </div>   
+
+      <ul>
+      {movies.map(movie => (
+        <li key={movie.name}>{movie.title}</li>
+      ))}
+      {movies.map(movie => (
+        <li>{movie.name}</li>
+      ))}
+    </ul>
     </>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,39 @@
+import { useState, useEffect } from "react";
+
+
+type resultProps = {
+    name: string;
+}
+
 const Home = () => {
-    return <p>Home</p>;
+    const [movie, setMovie] = useState<resultProps[]>([]);
+
+    useEffect(() => {
+        const api = async () => {
+          const data = await fetch("https://remarkable-bombolone-51a3d9.netlify.app/.netlify/functions/movies", {
+            method: "GET"
+          });
+          const jsonData = await data.json();
+          setMovie(jsonData.results);
+        };
+    
+        api();
+      }, []);
+
+    return (
+    <>  
+        <p>hi</p>
+        <h1>
+            {movie.map((value) => {
+            return (
+                <div>
+                <div>{value.name}</div>
+                </div>
+            );
+            })}
+        </h1>
+
+    </>
+    )
 }
 export default Home;
